2ba7bb363c9aca97b61cc3285d96d046c4b56ca7
maksim <letovlab@gmail.com>
2025-03-19 17:58:14 +0100

    layout fix

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-container class="pa-0 ma-0">
-    <app-header />
-    <v-main>
-      <v-container>
-        <router-view />
-      </v-container>
-    </v-main>
-    <app-footer />
-  </v-container>
+  <app-header />
+  <v-main>
+    <v-container
+      fluid
+      class="pa-0 fill-height"
+    >
+      <router-view />
+    </v-container>
+  </v-main>
+  <app-footer />
 </template>
 
 <script lang="ts" setup>
